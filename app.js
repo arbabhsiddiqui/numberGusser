35a92793753b3c7e5cf403b3645265619c6604ca
Arbab <zackh41@gmial.com>
2020-08-26 23:12:35 +0530

    add game over and number of guess left

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ guessBtn.addEventListener("click", function () {
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`please enter a number between ${min} and ${max}`, "red");
   }
+  //   check if won
   if (guess === winningNum) {
     //   disable input
     guessInput.disabled = true;
@@ -34,6 +35,22 @@ guessBtn.addEventListener("click", function () {
     guessInput.style.borderColor = "green";
     // set winning message
     setMessage(`${winningNum} is correct, You Win`, "green");
+  } else {
+    guessLeft -= 1;
+    if (guessLeft === 0) {
+      //   disable input
+      guessInput.disabled = true;
+      // guessinput border color
+      guessInput.style.borderColor = "red";
+      // set winning message
+      setMessage(`Game Over, winning number is ${winningNum}`, "red");
+    } else {
+      //   game continues - answer wrong
+      guessInput.style.borderColor = "red";
+
+      setMessage(`${guess} is not correct, ${guessLeft} Guesses Left`, "black");
+      guessInput.value = "";
+    }
   }
 });
 
